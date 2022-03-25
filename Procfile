f33911fa1390dbc8215e3c37910afb531aca53da
glinoen <juho.linden@gmail.com>
2022-03-25 17:50:49 +0200

    fix serving static build from backend

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run server && npm run serve
+web: node server.js
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server')
+const express = require('express')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3001
 
+server.use(express.static('build'))
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
